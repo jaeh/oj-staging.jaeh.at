9679d88ffe2bfb31a16933d914f90718be026eb8
Jascha Ehrenreich <jascha@jaeh.at>
2015-01-28 22:58:46 +0100

    fixed darklight.js to remember colors but only show gui in slide mode

--- public/js/bundle/darkLight.js
+++ public/js/bundle/darkLight.js
@@ -5,19 +5,19 @@ var utils = require('./utils');
 /*
  * rendering and adds event listeners for the dark/light button
 */
-function addGUI () {
-  var buttonContainer  = document.createElement('li')
-    , menuContainer    = utils.getMenuContainer()
+function addGUI (isSlidePage) {
+  var menuContainer    = utils.getMenuContainer()
     , menuUl           = menuContainer.querySelector('ul')
-    , button           = document.createElement('a')
     , timeString       = 'dark'
     , timeText         = 'light'
     , hours            = new Date().getHours()
     , body             = document.body
   ;
+
+
+
   if ( utils.localStorage() ) {
     timeString = localStorage.bodyClass;
-    
   }
 
   body.classList.remove('dark');
@@ -33,39 +33,45 @@ function addGUI () {
     localStorage.bodyClass = timeString;
   }
 
-  buttonContainer.classList.add('darklight');;
-  buttonContainer.classList.add('btn-container');
-  buttonContainer.classList.add(timeString);
-
-  //~ button.classList.add('icon-lamp');
-  button.innerHTML = timeText;
-  buttonContainer.appendChild(button);
-  menuUl.appendChild(buttonContainer);
-  //~ body.appendChild(buttonContainer);
-  //~ body.insertBefore(buttonContainer, body.firstChild);
-
-  button.addEventListener('click', function (evt) {
-    var oldClass = ( body.className.indexOf('dark') >= 0 ) ? 'dark' : 'light'
-      , newClass = ( oldClass === 'dark' ) ? 'light' : 'dark'
+  if ( isSlidePage ) {
+    var buttonContainer  = document.createElement('li')
+      , button           = document.createElement('a')
     ;
-    if ( utils.localStorage() ) {
-      localStorage.bodyClass = newClass;
-    }
-    evt.target.innerHTML = oldClass;
-    body.classList.remove(oldClass);
-    body.classList.add(newClass);
-  });
+
+    buttonContainer.classList.add('darklight');;
+    buttonContainer.classList.add('btn-container');
+    buttonContainer.classList.add(timeString);
+
+    //~ button.classList.add('icon-lamp');
+    button.innerHTML = timeText;
+    buttonContainer.appendChild(button);
+    menuUl.appendChild(buttonContainer);
+    //~ body.appendChild(buttonContainer);
+    //~ body.insertBefore(buttonContainer, body.firstChild);
+
+    button.addEventListener('click', function (evt) {
+      var oldClass = ( body.className.indexOf('dark') >= 0 ) ? 'dark' : 'light'
+        , newClass = ( oldClass === 'dark' ) ? 'light' : 'dark'
+      ;
+      if ( utils.localStorage() ) {
+        localStorage.bodyClass = newClass;
+      }
+      evt.target.innerHTML = oldClass;
+      body.classList.remove(oldClass);
+      body.classList.add(newClass);
+    });
+  }
 }
 
 
 (function addDarkLightUi() {
   var contentEle  = document.getElementById('content');
   if ( contentEle ) {
-    var cN = contentEle.className;
+    var cN          = contentEle.className
+      , isSlidePage = cN.indexOf('slide') >= 0
+    ;
     //only load gui on gallery page
-    if ( cN.indexOf('slide') >= 0 ) {
-      addGUI();
-    }
+    addGUI(isSlidePage);
   }
 })();
 
