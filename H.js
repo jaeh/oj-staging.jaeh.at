83cc62349e6c2287bbe10346cd24453d7068dd07
Jascha Ehrenreich <jascha@jaeh.at>
2015-02-25 19:51:32 +0100

    various layout changes

--- H.js
+++ H.js
@@ -10,4 +10,19 @@ H.enable('bodyParser');
 
 H.set('routes', router);
 
+H.use(function(req, res, next){
+  res.locals.menu = [
+    { href: '/',        name: 'index',   text: 'oliver jiszda' }
+  , { href: '/gallery', name: 'gallery', text: 'gallery' }
+  , { href: '/contact', name: 'contact', text: 'contact' }
+  , { 
+        href  : 'http://jiszda.tumblr.com'
+      , name  : 'blog'
+      , text  : 'blog'
+      , target: 'tumblr'
+    }
+  ];
+  next();
+});
+
 module.exports = H;
